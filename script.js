5b0374cf180295fb3a2d54583675d282c220731c
unknown <blink_182@inbox.ru>
2023-01-17 21:04:13 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let burger = document.querySelector('.header__burger');
+let header = document.querySelector('.header');
+let ul = document.querySelector('.header__nav-ul')
+let nav = document.querySelector('.header__nav');
+
+burger.addEventListener('click', ()=>{
+    burger.classList.toggle('is__active');
+    nav.classList.toggle('is__active');
+    ul.classList.toggle('is__active-menu-list');
+    header.classList.toggle('is__active-background');
+    
+})
